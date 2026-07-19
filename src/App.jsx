16fdf8b14f4d3ca4eaeb4f38754231e2194d46c7
Bello Abdulrahman Adebolaji <https://github.com/settings/connections/applications/0120e057bd645470c1ed>
2026-07-19 13:50:18 +0100

    i changed the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
 
-import Certificates from "./components/Certificates/Certificates";
-import ParticlesBackground from "./components/ParticlesBackground/ParticlesBackground";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
@@ -18,42 +16,53 @@ import ScrollTop from "./components/ScrollTop/ScrollTop";
 function App() {
   return (
     <div className="App">
-        <ParticlesBackground />
+
       <Navbar />
+
       <main>
+
         <section id="home">
           <Hero />
         </section>
+
         <section id="about">
           <About />
         </section>
+
         <section id="skills">
           <Skills />
         </section>
+
         <section id="projects">
           <Projects />
         </section>
+
         <section id="github">
           <Github />
         </section>
-        <section id="certificates">
-  <Certificates />
-</section>
+
         <section id="stats">
           <Stats />
         </section>
+
         <section id="timeline">
           <Timeline />
         </section>
+
         <section id="password-checker">
           <PasswordChecker />
         </section>
+
         <section id="contact">
           <Contact />
         </section>
+
       </main>
+
       <Footer />
+
       <ScrollTop />
+
     </div>
   );
 }
